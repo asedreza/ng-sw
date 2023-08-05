dba8c12d13c8821547f323595e0ce91536b3b5d4
r.asadollahi <seyedreza.asadollahi@gmail.com>
2023-08-05 21:36:18 +0330

    1402/05/14 - add dynamic caching feature - add cleanup for old caches

--- src/custom-worker.js
+++ src/custom-worker.js
@@ -1,4 +1,5 @@
-const STATIC_CACHE = 'STATIC_CACHE_V1'
+const STATIC_CACHE = 'STATIC_CACHE_V2';
+const DYNAMIC_CACHE = 'DYNAMIC_CACHE_V1';
 
 self.addEventListener('install', event => {
   console.log('installing Custom SW ...')
@@ -6,24 +7,42 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(STATIC_CACHE).then(cache => {
       console.log('caching static files ...')
-      cache.add('/assets/bootstrap.min.css')
+      cache.addAll([
+        '/assets/bootstrap.min.css',
+        '/assets/icons/icon-72x72.png',
+        '/assets/icons/icon-96x96.png',
+        '/assets/icons/icon-144x144.png',
+      ])
     })
   )
 })
 
 self.addEventListener('activate', event => {
-  console.log('installing Custom SW ...')
+  console.log('activating Custom SW ...')
+  event.waitUntil(
+    caches.keys().then(allKeys => Promise.all(allKeys.map(key => {
+      if(key !== STATIC_CACHE){
+        console.log('Removing old caches: ', key)
+        return caches.delete(key)
+      }
+    })))
+  )
   return self.clients.claim();
 })
 
 self.addEventListener('fetch', event => {
-  console.log('caching static files ...')
   event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        return response;
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        console.log('load from cache:', event.request)
+        return cachedResponse;
       } else {
-        return fetch(event.request)
+        return fetch(event.request).then(response => {
+          return caches.open(DYNAMIC_CACHE).then(cache => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          })
+        })
       }
     })
   )
